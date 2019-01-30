2db74451708d762b49a16d24e384428b802547b9
so <so.c.choi@gmail.com>
2019-01-30 14:48:52 -0800

    Add queue position such that first person to sign up is at position 1. Update frontend to reflect user's place in line as well as collective invitations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,23 @@ class UsersController < ApplicationController
     ref_code = cookies[:h_ref]
     email = params[:user][:email]
     @user = User.new(email: email)
-    @user.referrer = User.find_by_referral_code(ref_code) if ref_code
+    @duplicate_user = User.find_by_email(email)
 
+    # If the user has already signed up, just redirect them to the refer page.
+    if @duplicate_user
+      cookies[:h_email] = { value: email }
+      redirect_to '/refer-a-friend'
+      return
+    end
+
+    # If the user signed up via a referral code, move the referrer up the sign-up list by one.
+    @user.referrer = User.find_by_referral_code(ref_code) if ref_code
+    if @referrer then
+      @referrer.points = @referrer.points + 1
+      @referrer.save
+    end
+
+    # On each user's sign-up, save their current session.
     if @user.save
       cookies[:h_email] = { value: @user.email }
       redirect_to '/refer-a-friend'
@@ -33,6 +48,11 @@ class UsersController < ApplicationController
     @is_mobile = mobile_device?
 
     @user = User.find_by_email(cookies[:h_email])
+
+    if @user
+      @total_users = User.count
+      @queue_position = User.where("points >  ?", @user.points).count + User.where("points = ? AND updated_at < ?", @user.points, @user.updated_at).count + 1
+    end
 
     respond_to do |format|
       if @user.nil?
