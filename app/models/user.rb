77e1cfd82126276690f5093543a9f1a67d201b8b
so <so.c.choi@gmail.com>
2019-01-30 14:46:56 -0800

    Update user model to include current referrer and points

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,36 +13,7 @@ class User < ActiveRecord::Base
   before_create :create_referral_code
   after_create :send_welcome_email
 
-  REFERRAL_STEPS = [
-    {
-      'count' => 5,
-      'html' => 'Shave<br>Cream',
-      'class' => 'two',
-      'image' =>  ActionController::Base.helpers.asset_path(
-        'refer/cream-tooltip@2x.png')
-    },
-    {
-      'count' => 10,
-      'html' => 'Truman Handle<br>w/ Blade',
-      'class' => 'three',
-      'image' => ActionController::Base.helpers.asset_path(
-        'refer/truman@2x.png')
-    },
-    {
-      'count' => 25,
-      'html' => 'Winston<br>Shave Set',
-      'class' => 'four',
-      'image' => ActionController::Base.helpers.asset_path(
-        'refer/winston@2x.png')
-    },
-    {
-      'count' => 50,
-      'html' => 'One Year<br>Free Blades',
-      'class' => 'five',
-      'image' => ActionController::Base.helpers.asset_path(
-        'refer/blade-explain@2x.png')
-    }
-  ]
+  default_scope { order('points DESC') }
 
   private
 
