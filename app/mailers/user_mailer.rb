ab9ae485e38b3a83d85ac29fc45368e3d5020009
so <so.c.choi@gmail.com>
2019-01-30 18:00:01 -0800

    Update references to logo and replace references to Harry's with Knowledgr

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "Harry's <welcome@harrys.com>"
+  default from: "Knowledgr <team@knowledgr.co>"
 
   def signup_email(user)
     @user = user
-    @twitter_message = "#Shaving is evolving. Excited for @harrys to launch."
+    @twitter_message = "A scientific revolution is coming. Click here to join the waiting list, by invite only link."
 
     mail(:to => user.email, :subject => "Thanks for signing up!")
   end
